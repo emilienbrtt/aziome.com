3778ec307fee5dd25a06485f0ed2227e0b8c5518
emilienbrtt <baret.emilien@gmail.com>
2025-09-30 22:19:47 +0200

    Update Solutions.tsx

--- components/Solutions.tsx
+++ components/Solutions.tsx
@@ -78,7 +78,7 @@ export default function Solutions() {
               // IMPORTANT : object-contain pour voir l’animal en entier,
               // ancrage en bas, et un léger scale pour éviter qu’il paraisse “petit”.
               'object-contain object-bottom select-none transition duration-200',
-              role === 'center' ? 'scale-[1.03]' : 'scale-[1.01]'
+              role === 'center' ? 'scale-[1.05]' : 'scale-[1.03]'
             ].join(' ')}
           />
           {/* Dégradé vers le texte */}
